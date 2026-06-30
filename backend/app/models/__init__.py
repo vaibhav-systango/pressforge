11dd7bbbcb4c253a9ce8914481076fc59e16ddea
shruti-systango <shruti.shukla@systango.com>
2026-06-30 18:56:28 +0530

    Revert "feat: add roles, permissions tables and role seeder"
    This reverts commit 401b5f0198cf27b12b4774d0444ec70878289c30.

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -4,15 +4,11 @@ from app.models.user import User
 from app.models.organization import Organization
 from app.models.organization_kyc import OrganizationKyc
 from app.models.organization_member import OrganizationMember
-from app.models.role import Role, Permission, RolePermission
 
 __all__ = [
     "Base",
     "User",
     "Organization",
     "OrganizationKyc",
-    "OrganizationMember",
-    "Role",
-    "Permission",
-    "RolePermission"
+    "OrganizationMember"
 ]
